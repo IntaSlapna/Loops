23ce8791390316b3b1478057cb9bd1ac7c097632
Inta.Slapna <inta.slapnja@inbox.lv>
2021-10-13 22:59:41 +0300

    3rd assignment finished

--- src/com/company/AssignmentStudents.java
+++ src/com/company/AssignmentStudents.java
@@ -7,26 +7,68 @@ public class AssignmentStudents {
     public static void main(String[] args) {
         byte[] gradesArr = new byte[10];
 
-        for(int c=0; c<gradesArr.length; c++) {
-            System.out.println("Please enter the grade " +(c+1));
-            Scanner input = new Scanner(System.in);
-            gradesArr[c] = input.nextByte();
-            //awEif(gradesArr[c] >= 0 && <= 10)
+        int i = 0;
+        int orderNum = 1;
+
+        while (orderNum <= gradesArr.length) {
+            Scanner inputGrade = new Scanner(System.in);
+            System.out.println("Please enter grade the grade of student #" + orderNum);
+            byte grade = inputGrade.nextByte();
+            //Edgar, I still do not understand how to use this hasNextShort(), maybe you could show?
+
+            if (grade >= 0 && grade <= 10) {
+                gradesArr[i++] = grade;
+                orderNum++;
+            } else {
+                System.out.println("Incorrect grade. Grade is from 0 till 10. Try one more time!");
+            }
+            //input.close(); //WTF, why it is red again?
         }
-        //input.close(); why shows red if uncomment??
+
 
         System.out.println();
-        System.out.println("You entered theese grades:");
-        for(int c=0; c<gradesArr.length; c++) {
-            System.out.println(gradesArr[c]);
+        System.out.println("You entered following grades:");
+        for (int p = 0; p < gradesArr.length; p++) {
+            System.out.println(gradesArr[p]);
         }
 
-        byte i = 0;
-        while (i < gradesArr.length) {
 
-
+        System.out.println();
+        int countMult = 0;
+        for (int m = 0; m < gradesArr.length; m++) {
+            if (gradesArr[m] < 4) {
+                countMult++;
+            }
         }
+        System.out.println(countMult != 0 ? "The number of students who failed equals: " + countMult :
+                "There is no students who failed");
 
+
+        System.out.println();
+        int countMultA = 0;
+        for (int a = 0; a < gradesArr.length; a++) {
+            if (gradesArr[a] == 10) {
+                countMultA++;
+            }
+        }
+        System.out.println(countMultA != 0 ? "The number of students who got grade A equals: " + countMultA :
+                "There is no students who got the grade A");
+
+
+        System.out.println();
+        int gradehist = 0;
+        while (gradehist <= 10) {
+            int studNum = 0;
+            for (int stud : gradesArr) {
+                if (stud == gradehist) {
+                    studNum++;
+                }
+            }
+            System.out.println(studNum + " students with the grade " + gradehist);
+            gradehist++;
+        }
     }
 }
+
+
 
